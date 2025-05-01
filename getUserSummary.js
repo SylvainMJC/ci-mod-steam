72a9c61827c8cb7c4616ca4254ec9f04f364c6b4
Sylvain Conan <sylvain.mj.conan@gmail.com>
2025-05-01 11:37:00 +0200

    package

---
+++ getUserSummary.js
@@ -0,0 +1,12 @@
+import SteamAPI from 'steamapi';
+
+
+
+const steam = new SteamAPI('A228ED13FF86421919AC19B3E99C770D');
+
+const user = steam.getUserSummary('76561198041411600').then(summary => {
+	console.log(summary);
+})
+
+console.log(user);
+
